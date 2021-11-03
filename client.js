6ad2615cfeea926f0be79c20cb0909bbeb7c206b
Sofiya Lipova <sofiya.lipova@mail.mcgill.ca>
2021-11-03 22:39:32 +0000

    create index file and refactor

--- client.js
+++ client.js
@@ -22,7 +22,5 @@ const connect = function() {
 
 };
 
-console.log("Connecting ...");
-connect();
 
 module.exports = { connect }
--- play.js
+++ play.js
@@ -1,30 +1,7 @@
 const { connect } = require('./client.js');
+const { setupInput } = require('./input.js')
 
-
-// setTimeout(() => {
-//   conn.write("Move: up");
-// }, 1000)
-
-
-// setup interface to handle user input from stdin
-
-const setupInput = function() {
-  const stdin = process.stdin;
-  stdin.setRawMode(true);
-  stdin.setEncoding("utf8");
-  stdin.resume();
-
-  //works only if pressed twice
-  const handleUserInput = function() {
-    stdin.on('data', (key) => {
-      if (key === '\u0003') {
-        process.exit();
-      }
-    });
-  };
-
-  stdin.on("data", handleUserInput);
-  return stdin;
-};
+console.log("Connecting ...");
+connect();
 
 setupInput();
